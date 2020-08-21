73edb238f226554bd0a1c380226432a2fabf4c25
Majd Turfa <turfa@51nodes.io>
2020-08-21 10:15:25 +0200

    export parse and stringify methods

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { initLibrary, getConfig, registerSchema, getSchema } from './schema-regi
 export { SchemaType, Network, ConfigObject } from './model';
 export { InvalidInput } from './exceptions/invalid-input.exception';
 export { FailedToPin } from './exceptions/failed-to-pin.exception';
+export { parseSchemaDid, stringifySchemaDid} from './did/did-utils'
