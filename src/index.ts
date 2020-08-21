d519d8a766260da79994d76c7b073f38f358ed48
Majd Turfa <turfa@51nodes.io>
2020-08-21 10:18:58 +0200

    export SchemaDID model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { initLibrary, getConfig, registerSchema, getSchema } from './schema-registry';
-export { SchemaType, Network, ConfigObject } from './model';
+export { SchemaType, Network, ConfigObject, SchemaDid } from './model';
 export { InvalidInput } from './exceptions/invalid-input.exception';
 export { FailedToPin } from './exceptions/failed-to-pin.exception';
 export { parseSchemaDid, stringifySchemaDid} from './did/did-utils'
