b9b26e852eff1456bbddde397d383e51c78b34f5
Jan Paul Buchwald <buchwald@51nodes.io>
2020-09-24 18:21:45 +0200

    Reviewer adjustments

--- src/schema-types/json-schema-validator.ts
+++ src/schema-types/json-schema-validator.ts
@@ -3,7 +3,7 @@ import * as Validator from '@exodus/schemasafe';
 export function validateJsonSchemaType(schemaContent: string): boolean {
   // just try to load the schema for validation, if it fails, consider invalid
   try {
-    Validator.validator(JSON.parse(schemaContent))
+    Validator.validator(JSON.parse(schemaContent));
     return true;
   } catch (error) {
     return false;
