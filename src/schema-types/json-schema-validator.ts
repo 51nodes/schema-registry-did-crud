f583ea985de5424dbaea28bd35ae70aa3a05b71f
Majd Turfa <turfa@51nodes.io>
2020-09-24 12:05:41 +0200

    use another library for validation of json schema

--- src/schema-types/json-schema-validator.ts
+++ src/schema-types/json-schema-validator.ts
@@ -1,9 +1,9 @@
-import * as JsonSchema from '@hyperjump/json-schema';
+import * as Validator from '@exodus/schemasafe';
 
 export function validateJsonSchemaType(schemaContent: string): boolean {
   // just try to load the schema for validation, if it fails, consider invalid
   try {
-    JsonSchema.add(JSON.parse(schemaContent));
+    Validator.validator(JSON.parse(schemaContent))
     return true;
   } catch (error) {
     return false;
--- src/tests/schema-type.test.ts
+++ src/tests/schema-type.test.ts
@@ -52,6 +52,42 @@ test('Valid Json Schema returns true', () => {
   expect(validationResult).toBe(true);
 });
 
+test('Valid Json Schema without $id returns true', () => {
+  const validJsonSchema = `{
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "Product",
+        "description": "A product from Acme's catalog",
+        "type": "object",
+        "properties": {
+          "productId": {
+            "description": "The unique identifier for a product",
+            "type": "integer"
+          },
+          "productName": {
+            "description": "Name of the product",
+            "type": "string"
+          },
+          "price": {
+            "description": "The price of the product",
+            "type": "number",
+            "exclusiveMinimum": 0
+          },
+          "tags": {
+            "description": "Tags for the product",
+            "type": "array",
+            "items": {
+              "type": "string"
+            },
+            "minItems": 1,
+            "uniqueItems": true
+          }
+        },
+        "required": [ "productId", "productName", "price" ]
+      }`;
+  const validationResult = validateSchemaType(validJsonSchema, SchemaType.JsonSchema);
+  expect(validationResult).toBe(true);
+});
+
 test('Single String XSD returns false', () => {
   const validationResult = validateSchemaType('NoXsd', SchemaType.Xsd);
   expect(validationResult).toBe(false);
